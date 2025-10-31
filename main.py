8090c7fcace8a07e4e74c2d6f4b2f14714c79f22
Ashwini.Wakhare <ashwini.wakhare@facileserv.in>
2025-10-31 12:24:18 +0530

    fastapi Swagger UI

--- main.py
+++ main.py
@@ -1,11 +1,53 @@
+from typing import Optional
 from fastapi import FastAPI
+from pydantic import BaseModel
+import uvicorn
 
+app = FastAPI()
 
-app=FastAPI()
-@app.get('/')
+@app.get("/")
 def index():
-    return {'hiee':{"name":"ashwini"}}
+    return {"data": "Blog List"}
+
+# if __name__ == "__main__":
+#     uvicorn.run(app, host="127.0.0.1", port=9000)
+
+    
+    
+class Blog(BaseModel):
+    title:str
+    body:str
+    published:Optional[bool]
+        
+@app.post('/blog')
+def create_blog(request:Blog):
+    # return request
+    return{'data':f'blog is created as title with {request.title}'}
+
+
+
 
 @app.get('/about')
 def about():
     return {"data":"this is the about page"}
+
+@app.get('/blog/{id}')
+def show(id: int ):
+    return {'data':id}    
+
+@app.get('/blog/{id}/comments')
+def comments(id):
+    #fetch comments of blog with id =id
+    return {'data':{'1','2'}}
+
+@app.get('/blog')
+def index(limit=10,published:bool=True, sort:Optional[str]=None):
+    if published:
+        return{'data':f'{limit} published blogs are from database'}
+    else:
+        return{'data':f'{limit} all blogs are from database'}
+
+@app.get('/blog/unpublished')
+def unpublished():
+    return {'data':"this is unpublished blog"}
+
